b29e6252c8d96570d2942a74f4eebc4a9d57b003
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-10-25 18:18:13 +0100

    Parameterize the abort behaviour

--- snakepit/security.py
+++ snakepit/security.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from flask import g, session, abort
+from flask import g, session
 import bcrypt
 
 def hash_pw(password):
@@ -10,21 +10,19 @@ def test_pw(password, hashed):
 
 class Authenticator(object):
 
-    def __init__(self, connector):
+    def __init__(self, connector, on_fail):
         self.connect = connector
+        self.on_fail = on_fail
 
     def get_current_user_roles(self):
-        if not session["user"]:
-            print "No user variable on the session"
+        if not "user" in session:
             return set(["anon"])
         with self.connect() as store:
             u = store.fetch_user(**session["user"])
             if not u:
-                print "No user found"
                 session["user"] = None
                 return set(["anon"])
             roles = set(str(role.name) for role in u.roles)
-            print "Found:", roles
             return roles
 
     def requires_roles(self, *roles):
@@ -33,7 +31,7 @@ class Authenticator(object):
             @wraps(f)
             def wrapped(*args, **kwargs):
                 if not self.get_current_user_roles() & roles:
-                    return abort(403)
+                    return self.on_fail()
                 return f(*args, **kwargs)
             return wrapped
         return wrapper
